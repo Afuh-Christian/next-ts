0aa33249841c2bc33194eaf1d63d592c828edfd5
Afuh Christian <afuhchristian10@gmail.com>
2024-05-15 09:48:32 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import App from "@/App";
+
+
+export const metadata: Metadata = {
+  title: "Next App",
+  description: "Template .. ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <App>
+        {children} 
+        </App>
+        </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+import BasicList from "@/components/BasicList";
+import { Counter } from "@/components/Counter";
+import TemporaryDrawer from "@/components/TemporaryDrawer";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const router = useRouter()
+  return (
+    <>
+<Counter/>
+<div>Hello</div>
+
+<div onClick={()=>router.push("/about")}>
+  About
+</div>
+<div className="flex flex-col">
+<Link href="/about">about</Link>
+<Link href="/products">products</Link>
+<Link href="/services">services</Link>
+</div>
+   </>
+  );
+}
